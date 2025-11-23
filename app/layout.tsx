a34074b874e9a25c1a355befeb875c845d753033
chayen <romory@pop-os.CG-24D>
2025-11-23 10:24:51 +0800

    Final setup before development

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+// @ts-ignore: allow importing global CSS without type declarations
+import "@/app/styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
