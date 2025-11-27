134d220ca35b14754eb6bf7caf4f04e10f4d3b8a
xRomory <clyncemiro@gmail.com>
2025-11-28 00:01:24 +0800

    Working on BookingReservation components

--- frontend/src/pages/BookingPage.jsx
+++
@@ -1,9 +0,0 @@
-import UserInformation from "@/components/UserInformation/UserInformation";
-
-export default function BookingPage() {
-  return (
-    <div>
-      <UserInformation />
-    </div>
-  )
-}
