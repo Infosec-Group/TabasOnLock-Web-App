0af218f7327de2eb0495fcd1ce9a0ab44123bed5
xRomory <clyncemiro@gmail.com>
2025-12-08 12:31:43 +0800

    feat(auth): Fixed CORS origin connection, and fix auth connection

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,7 @@ app.use(
     origin: process.env.CORS_ORIGIN || "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   })
 );
 app.use(express.json());
