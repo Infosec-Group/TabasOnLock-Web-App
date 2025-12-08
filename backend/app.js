e3d7ac3f632c229df40a9af8a62860ac5144af37
xRomory <clyncemiro@gmail.com>
2025-12-09 00:04:28 +0800

    Cancel and Delete Booking done.

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN || "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
